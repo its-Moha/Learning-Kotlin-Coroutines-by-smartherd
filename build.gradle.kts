400cf302f530def986e3a092a13ab328315bf21d
mohamedamin <funstars008@gmail.com>
2025-12-13 12:26:33 +0300

    threads and doing our first coroutine

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 }
 
 kotlin {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,44 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
+import com.sun.source.util.Plugin
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+fun main() { // Executes on The main thread
+
+    println("Main program starts: ${Thread.currentThread().name}")
+
+
+
+    GlobalScope.launch { //creates a background coroutine that runs on the background thread
+        println("Fake work 2 starts: ${Thread.currentThread().name}")
+        delay(4000)
+        println("Fake work 2 finishes: ${Thread.currentThread().name}")
     }
+
+    // block the current main thread and wait for the coroutine to finish
+    Thread.sleep(6000)
+
+    println("Main Program Ends: ${Thread.currentThread().name}")
 }
+
+
+
+
+
+//when the user launches the app a default thread is created
+// and that thread is Known as main thread, and it is the life
+// of the application
+// and its operation is to perform some operations,
+// and we perform it in small operation like click of a button or ui interaction
+// but if we run big operations like network,downloading or load image
+// our main thread will be blocked and our app will freeze
+//so we need a worker thread or a background thread but if you
+// do five of them our device will run out of memory and our app will crash
+// That is where coroutines come you will have one background thread
+//to launch a coroutine with different coroutines
+//they are lightweight threads and can run in parallel
+//  coroutines != threads
