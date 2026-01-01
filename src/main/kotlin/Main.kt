e2fe060fc8c003461e1b86644ba3bd2f1d267a89
mohamedamin <funstars008@gmail.com>
2026-01-01 13:56:55 +0300

    redoing first chapter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,23 +4,29 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
 
 
 fun main() { // Executes on The main thread
 
     println("Main program starts: ${Thread.currentThread().name}")
 
+    thread {
+        println("Tread background work Starts : ${Thread.currentThread()}")
+        Thread.sleep(1000)
+        println("Tread background work Finishes : ${Thread.currentThread()}")
+    }
 
 
     GlobalScope.launch { //creates a background coroutine that runs on the background thread
         println("Fake work 2 starts: ${Thread.currentThread().name}")
-        delay(1000) // does not block which is it operating
+        delay(2000) // does not block which is it operating
         println("Fake work 2 finishes: ${Thread.currentThread().name}")
     }
 
     // block the current main thread and wait for the coroutine to finish
     runBlocking {
-        mySuspendFun(2000)
+        mySuspendFun(3000)
     }
 
     println("Main Program Ends: ${Thread.currentThread().name}")
@@ -35,6 +41,21 @@ suspend fun mySuspendFun(time: Long){
 //A coroutine is a smart job that can pause and continue later
 // without blocking everything.
 
+// coroutine Builders are used for creating coroutines
+
+//launch = fire and forget
+//async = wait for result
+//runBlocking = stop everything (bad)
+
+//GlobalScope = creates coroutine at global(app) level
+//e.g = file download/play music
+//Why it’s BAD ❌
+// Lives forever, Ignores screen lifecycle, Causes memory leaks, Can crash apps
+//🚫 Never use in Android UI
+
+//Launch = creates coroutine at local level
+//e.g = login operation
+
 //when the user launches the app a default thread is created
 // and that thread is Known as main thread, and it is the life
 // of the application
@@ -46,5 +67,11 @@ suspend fun mySuspendFun(time: Long){
 // do five of them our device will run out of memory and our app will crash
 // That is where coroutines come you will have one background thread
 //to launch a coroutine with different coroutines
-//they are lightweight threads and can run in parallel
+
+//they are lightweight threads and can run in parallel wait for each other
+//and communicate with each other
+
 //  coroutines != threads
+
+// with the memory conception of one thread using coroutines
+// you can perform so many operations
