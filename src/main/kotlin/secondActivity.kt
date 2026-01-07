0444404af7a58dce5c8bda7dda32b877b9fc889d
mohamedamin <funstars008@gmail.com>
2026-01-07 17:13:38 +0300

    chapter 3 m p2 of making our code ake the coroutine cooperative by using yield also active

--- src/main/kotlin/secondActivity.kt
+++ src/main/kotlin/secondActivity.kt
@@ -7,14 +7,19 @@ fun main() = runBlocking {// create a blocking coroutine
 
     println("Main Program Starts: ${Thread.currentThread()}")
 
-    val job : Job = launch { // Thread t1: creates a non-blocking coroutine
+    val job : Job = launch(Dispatchers.IO) { // Thread t1: creates a non-blocking coroutine
         for (i in 0..500){
+            if (!isActive){
+                return@launch
+            //break
+            }
             println("$i.")
-            delay(100)
+
+            Thread.sleep(1)
         }
     }
 
-    delay(1000) // let's print a few values before we cancel
+    delay(100) // let's print a few values before we cancel
     job.cancelAndJoin() // waits for the coroutine to finish
 
     println("Main Program Finishes: ${Thread.currentThread()}")
