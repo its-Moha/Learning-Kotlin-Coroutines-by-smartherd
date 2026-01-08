b4cc913eb161614c41a371b5854d48e39e01728e
mohamedamin <funstars008@gmail.com>
2026-01-08 17:10:27 +0300

    chapter 3 exception handling

--- src/main/kotlin/secondActivity.kt
+++ src/main/kotlin/secondActivity.kt
@@ -8,19 +8,32 @@ fun main() = runBlocking {// create a blocking coroutine
     println("Main Program Starts: ${Thread.currentThread()}")
 
     val job : Job = launch(Dispatchers.IO) { // Thread t1: creates a non-blocking coroutine
-        for (i in 0..500){
-            if (!isActive){
-                return@launch
-            //break
-            }
-            println("$i.")
 
-            Thread.sleep(1)
+        try {
+
+        for (i in 0..500){
+           // if (!isActive){ //     return@launch //break //  }
+            print("$i.")
+            delay(5)
+           // Thread.sleep(1)
         }
+            }catch (ex: CancellationException){
+                println("\nException caught safely: ${ex.message}")
+            }finally {
+            withContext(NonCancellable) {
+
+
+                delay(1000)
+                println("\nChoose a resources in finally")
+            }
+        }
+
+
     }
 
     delay(100) // let's print a few values before we cancel
-    job.cancelAndJoin() // waits for the coroutine to finish
+    job.cancel(CancellationException("my own crush massage"))
+    job.join() // waits for the coroutine to finish
 
     println("Main Program Finishes: ${Thread.currentThread()}")
 
